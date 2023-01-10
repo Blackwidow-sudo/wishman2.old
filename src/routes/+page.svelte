a08baa983cbdd617f36c2adeb0dc324af0019fa0
Merlin Cornehl <mcornehl@mkw.eu>
2023-01-10 22:16:32 +0100

    impl index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-  import { page } from '$app/stores'
+	import { page } from '$app/stores'
+	import PageTransition from '$lib/components/PageTransition.svelte'
 </script>
 
-{#if !$page.data.session}
-  <h1>I am not logged in</h1>
-{:else}
-  <h1>Welcome {$page.data.session.user.email}</h1>
-  <p>I am logged in!</p>
-{/if}
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<PageTransition>
+	{#if !$page.data.session}
+		<h1>I am not logged in</h1>
+	{:else}
+		<h1>Welcome {$page.data.session.user.email}</h1>
+		<p>I am logged in!</p>
+	{/if}
+</PageTransition>
