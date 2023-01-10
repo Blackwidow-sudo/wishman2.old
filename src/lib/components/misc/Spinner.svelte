3e3fe5e561a3e6f3a601f6c6ecee25bcd62b3be9
Merlin Cornehl <mcornehl@mkw.eu>
2023-01-10 22:19:00 +0100

    impl Spinner component

---
+++ src/lib/components/misc/Spinner.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	export let size: 'sm' | 'md' | 'lg'
+
+	let spinner: HTMLSpanElement
+
+	const sizeMap = {
+		sm: 24,
+		md: 48,
+		lg: 96
+	}
+
+	onMount(() => {
+		const mapped = sizeMap[size]
+		const borderWidth = Math.floor(mapped / 6).toString()
+
+		spinner.style.width = `${mapped}px`
+		spinner.style.height = `${mapped}px`
+		spinner.style.borderWidth = `${borderWidth}px`
+	})
+</script>
+
+<span
+	bind:this={spinner}
+	class="loader" />
+
+<style lang="scss">
+	.loader {
+		display: block;
+		border-style: solid;
+		border-color: #f3f3f3;
+		border-top-color: #3498db;
+		border-radius: 50%;
+		animation: spin 1s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
