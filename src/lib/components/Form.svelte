f9eab75f4bdb08fde95719be639b29afb2206198
Merlin Cornehl <mcornehl@mkw.eu>
2023-01-10 21:56:53 +0100

    impl Form component

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import SubmitButton from './buttons/SubmitButton.svelte'
+
+	/**
+	 * Props.
+	 */
+	export let title: string
+	export let loading: boolean = false
+</script>
+
+<form
+	class="flex flex-col gap-3 max-w-sm mx-auto"
+	{...$$restProps}
+	on:submit|preventDefault>
+	<fieldset class="flex flex-col gap-2">
+		<legend class="mb-4">
+			<h2 class="text-3xl font-medium">{title}</h2>
+		</legend>
+		<slot />
+		<slot name="actions">
+			<SubmitButton
+				{loading}
+				block
+				center>Submit</SubmitButton>
+		</slot>
+	</fieldset>
+</form>
