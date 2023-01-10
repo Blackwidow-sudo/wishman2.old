671bea4955f4aff2e512f62434f858e74e10799a
Merlin Cornehl <mcornehl@mkw.eu>
2023-01-10 21:49:50 +0100

    add @tailwind/forms package

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,svelte,js,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				md: '0 4px 8px var(--tw-shadow-color)',
+				lg: '0 8px 16px var(--tw-shadow-color)'
+			}
+		}
 	},
-	plugins: []
+	plugins: [
+		require('@tailwindcss/forms'),
+		plugin(({ matchUtilities, theme }) => {
+			matchUtilities(
+				{ 'text-shadow': val => ({ textShadow: val }) },
+				{ values: theme('textShadow') }
+			)
+		})
+	]
 }
